cd98e764bb1939949551cb8c939ec75654d100a1
kunkaffa <kunkaffa3@gmail.com>
2025-10-30 17:41:16 +0700

    Update run.py

--- run.py
+++ run.py
@@ -49,8 +49,7 @@ def display_header():
     print(f"{Fore.CYAN}|{'=' * 74}|")
 
 # Meminta IP target
-try:
-    ‎ip = int(input("Enter the target IP: ")
+‎ip = int(input("Enter the target IP: ")
 try:
     port = int(input("Enter the target port: "))
 except ValueError:
