06a939554af523023e986fc75a689325efd06776
kunkaffa <kunkaffa3@gmail.com>
2025-10-30 20:09:51 +0700

    Update run.py

--- run.py
+++ run.py
@@ -17,6 +17,7 @@ def clear():
     os.system("cls" if os.name == "nt" else "clear")
     
 # Fungsi untuk Menampilkan Header BASE dengan Warna
+os.system("clear")
 def display_header():
     header_lines = [ 
     f"{Fore.GREEN} ██    ",
