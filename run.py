c65c3e97f17feb8883325ce0a4be7902735b378a
kunkaffa <kunkaffa3@gmail.com>
2025-10-31 00:34:34 +0700

    Update run.py

--- run.py
+++ run.py
@@ -46,13 +46,13 @@ print(f"\033[97m╚{'═' * 70}╝")
 
 ‎‎# Password authentication function
 def authenticate():
-    ‎password = "BASe" # The password to access the tool
-    ‎user_password = getpass.getpass(prompt="\033[1;36mEnter the password to access the tool: \033[0m")
+    password = "kun313" # The password to access the tool
+    user_password = getpass.getpass(prompt="\033[1;36mEnter the password to access the tool: \033[0m")
 ‎
-   if user_password != password:
-       ‎print("\033[1;31mIncorrect password. Exiting...\033[0m")
+    if user_password != password:
+       print("\033[1;31mIncorrect password. Exiting...\033[0m")
        attemps += 1
-       ‎exit()
+       exit()
 # Meminta IP target
 ip = input("Enter the target IP: ")
 try:
