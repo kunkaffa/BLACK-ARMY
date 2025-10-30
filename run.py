e8fec766d4b0ae590e856f76d86e011fbd59a030
kunkaffa <kunkaffa3@gmail.com>
2025-10-30 16:07:13 +0700

    Update run.py

--- run.py
+++ run.py
@@ -45,17 +45,14 @@ logo = """
 \033[32m╚════════          JANGAN DENDAMAN MAS BRE...       ════════╝
 
 """
-‎‎‎‎while attemps < 100:
-    username = input("\033[32mEnter your username: \033[0m")
-    password = input("\033[31mEnter your password: \033[0m")
-
-    if username == 'bp4' and password == 'bp4':
-        print("\033[32m⟩⟩ Hai...! Welcome to zona attack BLACKPHANTER \033[0m")
-        break
-    else:
-        print('Incorrect credentials. Check if you have Caps lock on and try again.')
-        attemps += 1
-        continue
+‎‎‎‎‎def authenticate():
+    ‎password = "BASe" # The password to access the tool
+    ‎user_password = getpass.getpass(prompt="\033[1;36mEnter the password to access the tool: \033[0m")
+‎
+   if user_password != password:
+       ‎print("\033[1;31mIncorrect password. Exiting...\033[0m")
+       attemps += 1
+       ‎exit()
 ‎
 # Meminta IP dan port target
 ip = input("Enter the target IP: ")
